a194499e0ec3c7fc384e01ef3cc8656174396cf0
Ilaria Diliberto <ilaria.dil@gmail.com>
2026-03-18 14:51:19 +0100

    add_users folder

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-if-missin
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
